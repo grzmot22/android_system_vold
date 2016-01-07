178993da13156c942c29a1542d8e95febd012e15
Park Ju Hyung <qkrwngud825@gmail.com>
2016-01-07 21:52:10 +0900

    Do not abort on setexeccon failure
    User can be running the system without SELinux.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- Utils.cpp
+++ Utils.cpp
@@ -270,12 +270,10 @@ status_t ForkExecvp(const std::vector<std::string>& args, security_context_t con
 
     if (setexeccon(context)) {
         LOG(ERROR) << "Failed to setexeccon";
-        abort();
     }
     status_t res = android_fork_execvp(argc, argv, NULL, false, true);
     if (setexeccon(nullptr)) {
         LOG(ERROR) << "Failed to setexeccon";
-        abort();
     }
 
     free(argv);
@@ -302,12 +300,10 @@ status_t ForkExecvp(const std::vector<std::string>& args,
 
     if (setexeccon(context)) {
         LOG(ERROR) << "Failed to setexeccon";
-        abort();
     }
     FILE* fp = popen(cmd.c_str(), "r");
     if (setexeccon(nullptr)) {
         LOG(ERROR) << "Failed to setexeccon";
-        abort();
     }
 
     if (!fp) {
